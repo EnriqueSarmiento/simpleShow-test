2c8cb32e99fb0af8031b17fc6ec5857c006ca644
Enrique Sarmiento <jeisonsarmiento@correo.unimet.edu.ve>
2022-07-26 18:37:28 -0400

    first commit for this assessment

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+/**
+ * EXAMPLE CONTENT
+ * Feel free to remove it.
+ */
+
+import introImage from '../assets/example.svg';
+
+document.querySelector('#app').innerHTML = `
+   <h1>Hello there!</h1>
+   <img class="intro-image" src="${introImage}" alt="" />
+ `;
